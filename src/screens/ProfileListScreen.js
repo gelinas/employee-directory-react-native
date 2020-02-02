77da719e6ab4563e4f9ecf9a672c1c0c2fc60c2c
gelinas <jlgelinas@gmail.com>
2020-02-02 11:58:27 -0500

    network data instead of dummy data

--- src/screens/ProfileListScreen.js
+++ src/screens/ProfileListScreen.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FlatList, View, TouchableHighlight } from 'react-native'
 import ProfileCard from '../components/ProfileCard';
+import axios from 'axios';
 
 // Styles
 import styles from './styles/ProfileScreenStyles'
 import { Colors } from '../themes/'
 
 // dummy data
-import dummyData from '../prototype/data'
+// import dummyData from '../prototype/data'
 
 export default function ProfileListScreen({navigation}) {
+
+  const [profileList, setProfileList] = useState([])
+
+  useEffect(() => {
+      axios.get(
+        'https://directory-rest.herokuapp.com/api/directory'
+      ).then(res => {
+        setProfileList(res.data)
+        // console.log(res)
+      }).catch(err => {
+        // console.log(err);
+      });
+    }, []);
+
   return (
     <View style={styles.mainContainer}>
       {/* prototype FlatList */}
       <View style={styles.container}>
           <FlatList
-            data={dummyData}
+            // data={dummyData}
+            data={profileList}
             renderItem={({item}) => 
               <TouchableHighlight
                 underlayColor={Colors.steel}
