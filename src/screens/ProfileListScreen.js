52f379dc85eb1f7fbc9645298f9a04d60aef3a85
gelinas <jlgelinas@gmail.com>
2020-02-03 01:00:31 -0500

    update profile list on each load

--- src/screens/ProfileListScreen.js
+++ src/screens/ProfileListScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { FlatList, SafeAreaView, View, TouchableHighlight } from 'react-native'
+import { Alert, FlatList, SafeAreaView, Text, View, TouchableHighlight } from 'react-native'
+import { withNavigationFocus } from 'react-navigation';
 import ProfileCard from '../components/ProfileCard'
 import {axiosWithAuth} from '../utils/axiosWithAuth'
 
@@ -10,20 +11,26 @@ import { Colors } from '../themes/'
 // dummy data
 // import dummyData from '../prototype/data'
 
-export default function ProfileListScreen({navigation}) {
+function ProfileListScreen({navigation, isFocused}) {
 
   const [profileList, setProfileList] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [isError, setError] = useState(false)
 
   useEffect(() => {
+    if(isFocused){
       axiosWithAuth().get(
         '/directory'
       ).then(res => {
+        setLoading(false)
         setProfileList(res.data)
-        // console.log(res)
       }).catch(err => {
-        // console.log(err);
+        const {response} = err;
+        setError(true)
+        Alert.alert(`Error status code "${response.status}" with message "${response.data.message}" and error "${response.data.error}"`)
       });
-    }, []);
+    }
+    }, [isFocused]);
 
   return (
     <SafeAreaView style={styles.mainContainer}>
@@ -53,3 +60,5 @@ export default function ProfileListScreen({navigation}) {
     </SafeAreaView>
   )
 }
+
+export default withNavigationFocus(ProfileListScreen)
